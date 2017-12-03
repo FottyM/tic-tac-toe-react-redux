89e927cddc4f42c3438e3ed449c1ca9d1d54c8dc
Fortunat Mutunda <fotimutunda@gmail.com>
2017-12-03 21:15:14 +0200

    render squares on board

--- src/Board.js
+++ src/Board.js
@@ -13,7 +13,23 @@ export default class Board extends Component{
     render(){
         const status = 'Next: X';
         return(
-            <div className="Board">
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquares(0)}
+                    {this.renderSquares(1)}
+                    {this.renderSquares(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquares(3)}
+                    {this.renderSquares(4)}
+                    {this.renderSquares(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquares(6)}
+                    {this.renderSquares(7)}
+                    {this.renderSquares(8)}
+                </div>
 
             </div>
         );
