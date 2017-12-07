bc7de777ce5e5015fbe16070bc7cc9e86709c989
Fortunat Mutunda <fotimutunda@gmail.com>
2017-12-07 07:34:47 +0200

    getting there

--- src/index.js
+++ src/index.js
@@ -1,41 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider, connect } from 'react-redux'
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+
+
 
 import './index.css';
 import Game from './containers/Game';
+import store from './store';
 
-const mathReducer = (state = {
-    result: 1,
-    lastValues: [],
-}, action) => {
-    switch (action.type) {
-        case "ADD":
-            state = {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        case "SUBTRACT":
-            state = {
-                ...state,
-                result: state.result - action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        default:
-            state = {...state}
-
-    }
-    return state;
-};
-
-
-
-const store = createStore(combineReducers({mathReducer}), {}, applyMiddleware(logger));
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import  thunk from 'redux-thunk'
+
+import turnsReducer from './reducers/turnsReducer'
+
+export default createStore(
+    combineReducers({turnsReducer}),
+    {},
+    applyMiddleware(logger, thunks)
+);
